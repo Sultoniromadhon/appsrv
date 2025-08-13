bc81e02329eb047263abe69a29fbb45f6a7964d2
Sultoniromadhon <sultoniromadhan16@gmail.com>
2025-08-13 09:13:58 +0700

    unknown

--- app/Livewire/Pages/Domains/Index.php
+++ app/Livewire/Pages/Domains/Index.php
@@ -39,7 +39,7 @@ class Index extends Component
     {
         $this->resetErrorBag();
         $this->reset_form();
-        $this->dispatch('modal-show', ['modal' => $this->modal]);
+        $this->dispatch('modal-show', modal: $this->modal);
 
 
     }
